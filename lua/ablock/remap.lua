fc2253a931dc2e2830322a0cb1e713998cacb854
Alex Block <alex+forgejo@alexblock.io>
2026-06-04 14:08:54 -0500

    Removed sessionizer keybind

--- lua/ablock/remap.lua
+++ lua/ablock/remap.lua
@@ -29,7 +29,6 @@ vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-S-k>", "<cmd>cnext<CR>zz")
